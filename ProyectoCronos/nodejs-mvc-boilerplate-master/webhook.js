f2cbee24ec0618c403d71bdcf0d771170e45c5b9
Aaron3312 <a01642529@tec.mx>
2024-06-05 03:11:30 -0600

    finalemente

--- ProyectoCronos/nodejs-mvc-boilerplate-master/webhook.js
+++ ProyectoCronos/nodejs-mvc-boilerplate-master/webhook.js
@@ -8,20 +8,15 @@ app.use(express.json());
 
 app.post('/webhook', (req, res) => {
   console.log('Webhook received!');
-  const { ref } = req.body;
-  if (ref === 'refs/heads/main') {  // Cambia 'main' por tu rama principal si es diferente
-    exec('cd /home/aypierre225/ProyectoCronos05/CronosProyect1/ProyectoCronos/nodejs-mvc-boilerplate-master && git pull && npm install && pm2 restart all', (err, stdout, stderr) => {
-      if (err) {
-        console.error(`exec error: ${err}`);
-        return res.sendStatus(500);
-      }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-      res.sendStatus(200);
-    });
-  } else {
+  exec('cd /home/aypierre225/ProyectoCronos05/CronosProyect1/ProyectoCronos/nodejs-mvc-boilerplate-master && git fetch --all && git reset --hard origin/master && npm install && pm2 restart all', (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      return res.sendStatus(500);
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
     res.sendStatus(200);
-  }
+  });
 });
 
 app.listen(port, () => {
