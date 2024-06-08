94cb92f2eed1343e29305bd73aef2bcb243c2641
Aaron3312 <a01642529@tec.mx>
2024-06-08 11:37:47 -0600

    BetterFooter

--- doc/CronosApiDocs/main.js
+++ doc/CronosApiDocs/main.js
@@ -4,4 +4,9 @@ var app = express();
 var docs_handler = express.static(__dirname + '/docs/');
 app.use(docs_handler);
 //  start the server
-app.listen(8002);
+
+
+
+app.listen(8002, function() {
+    console.log("server is running on port 8002");
+});
